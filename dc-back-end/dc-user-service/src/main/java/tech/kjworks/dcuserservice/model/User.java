8bfb4046fc75b2d61252ba2be7246502abe8b506
KJ <duel.kj@gmail.com>
2022-10-11 23:51:15 +0300

    User model, getter/setter annotations instead of methods

--- dc-back-end/dc-user-service/src/main/java/tech/kjworks/dcuserservice/model/User.java
+++ dc-back-end/dc-user-service/src/main/java/tech/kjworks/dcuserservice/model/User.java
@@ -4,8 +4,12 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.Setter;
 import tech.kjworks.dcbaseservice.model.BaseModel;
 
+@Getter
+@Setter
 @Document(collection = "users")
 public class User extends BaseModel {
     @NotNull
@@ -17,22 +21,6 @@ public class User extends BaseModel {
     public User(String id, String firstName, String lastName) {
         super(id);
         this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
