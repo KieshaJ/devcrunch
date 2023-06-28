ecde1b3d03b1032043d66cee739c89e1d60057e5
Kestutis Jakseboga <kestutis.jakseboga@tradimo.com>
2023-06-28 13:45:30 +0300

    DC / More unneeded annotations removed

--- dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/repository/ContentBlockRepository.java
+++ dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/repository/ContentBlockRepository.java
@@ -1,11 +1,8 @@
 package tech.kjworks.dcpostservice.repository;
 
-import org.springframework.stereotype.Repository;
-
 import tech.kjworks.dcbaseservice.repository.BaseRepository;
 import tech.kjworks.dcpostservice.model.ContentBlock;
 
-@Repository
 public interface ContentBlockRepository extends BaseRepository<ContentBlock> {
     
 }
--- dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/repository/PostRepository.java
+++ dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/repository/PostRepository.java
@@ -1,14 +1,10 @@
 package tech.kjworks.dcpostservice.repository;
 
-import org.springframework.stereotype.Repository;
-
 import tech.kjworks.dcbaseservice.repository.BaseRepository;
 import tech.kjworks.dcpostservice.model.Post;
 import java.util.List;
 import tech.kjworks.dcpostservice.util.enums.PostType;
 
-
-@Repository
 public interface PostRepository extends BaseRepository<Post> {
     public List<Post> findByTitleContaining(String title);
     public List<Post> findByType(PostType type);
