7bd2f4c4b69e409ca855cdb772643a5a35072f62
KJ <duel.kj@gmail.com>
2023-07-21 22:46:42 +0300

    DC / Fix MongoConfig in post service

--- dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/config/MongoConfig.java
+++ dc-back-end/dc-post-service/src/main/java/tech/kjworks/dcpostservice/config/MongoConfig.java
@@ -3,13 +3,16 @@ package tech.kjworks.dcpostservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
+@EnableMongoRepositories("tech.kjworks.dcpostservice.repository")
 public class MongoConfig {
     @Bean
-    ValidatingMongoEventListener validatingMongoEventListener() {
-        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+    ValidatingMongoEventListener validatingMongoEventListener(
+        final LocalValidatorFactoryBean factory
+    ) {
         return new ValidatingMongoEventListener(factory);
     }
 }
