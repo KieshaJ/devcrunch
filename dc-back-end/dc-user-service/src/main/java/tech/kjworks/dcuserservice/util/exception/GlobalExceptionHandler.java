80c9ab145eccb19be422258af71fa47cbae4b530
KJ <duel.kj@gmail.com>
2023-07-22 23:15:28 +0300

    DC / Add global exception handlers to rest services

---
+++ dc-back-end/dc-user-service/src/main/java/tech/kjworks/dcuserservice/util/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package tech.kjworks.dcuserservice.util.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import tech.kjworks.dcbaseservice.util.exception.BaseExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends BaseExceptionHandler {
+    
+}
